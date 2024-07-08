2bf21e8eeff25432651135cbcac264f1050e989e
DanielZDF <131043093+DanielZDF@users.noreply.github.com>
2024-07-07 22:51:23 -0400

    cambio del codigo
    vaina

--- programas/Juego_Simulacion.py
+++ programas/Juego_Simulacion.py
@@ -3,61 +3,50 @@ import random
 # A = Apuesta usada
 # M = Monto de entrada
 # S = Numero maximo de simulaciones
-# R = Numero maximo de rondas por cada simulacion
 def juego_simulacion(A, M, S):
 
-    def juego(A, M):
-        i = 0
+    def juego (A, M):
         while True:
             M=M-A
             NumDado = random.randint(1, 6)
             if (NumDado % 2 == 0):
                 M=M+(A*2)
-            if (M <= -100 or M >= 500 or i == 200):
-                return M
+            if M <= -100:
+                return [M,0]
+            if M >= 500:
+                return [M,1]
 
-    def contador_resultados(L):
-        Victorias=0
-        Derrotas=0
-        Total=0
-        p=0
-        while True:
-            p= L.pop(0)
-            if p <= -100:
-                Derrotas=Derrotas+1
-            if p >= 500:
-                Victorias=Victorias+1
-            if L == []:
-                break
-        Total = Victorias + Derrotas
-        R = []
-        PorcentajeV = 100 * (Victorias/Total)
-        PorcentajeD = 100 * (Derrotas/Total)
-        return [PorcentajeV, PorcentajeD]
+    def resumen_resultados (L):
+        for Aux in L:
+            if Aux[1] == 1:
+                T = "Ganó"
+            else:
+                T = "Perdió"
+            print (str(Aux[0]) + " | " + T)
+
+    def calculo (L):
+        V=0
+        D=0
+        MF=0
+        for Aux in L:
+            MF=MF+Aux[0]
+            if Aux[1] == 0:
+                D=D+1
+            else:
+                V=V+1
+        print("No. de Victorias = " + str(V))
+        print("No. de Derrotas  = " + str(D))
+        print("Monto final  = " + str(MF))
+        
     
-    
-    i=0
-    lista_resultados=[]
+    Res = []
+    i = 0
     while True:
         i=i+1
-        lista_resultados.append(juego(A,M))
+        Res.append(juego(A, M))
         if i == S:
             break
-    print(lista_resultados)
-    resultado_porcentajes = contador_resultados(lista_resultados)
-    print("% de Victorias = "+ str(resultado_porcentajes.pop(0)))
-    print("% de Derrotas = "+ str(resultado_porcentajes.pop(0)))
+    resumen_resultados(Res)
+    calculo(Res)
 
-print("para jugar inserte los siguientes valores:")
-while True:
-    print("Apuesta usada (minimo 5$):")
-    Apuesta = int(input())
-    if Apuesta >= 5:
-        break
-    else:
-        print("ERROR: el minimo es 5$")
-print("Monto inicial:")
-MontoIni = int(input())
-print("Numero maximo de simulaciones:")
-MaxSim = int(input())
-juego_simulacion(Apuesta,MontoIni,MaxSim)
+juego_simulacion(100,0,50)
